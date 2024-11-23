ce3386ac6856186d86d3cce045c75e14e4d61dc4
JavaChrist <97617119+JavaChrist@users.noreply.github.com>
2024-11-23 19:20:25 +0100

    Responsive du site

---
+++ diplome.js
@@ -0,0 +1,59 @@
+ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+
+          if (this.getAttribute('href') === '#') {
+            // Scroll to the top of the page
+            window.scrollTo({
+              top: 0,
+              behavior: 'smooth'
+            });
+            return;
+          }
+
+          const targetID = this.getAttribute('href').substring(1);
+          const targetElement = document.getElementById(targetID);
+
+          if (targetElement) {
+            // Define different offsets for different sections
+            let headerOffset = 150; // Default offset
+
+            if (targetID === 'skills') {
+              headerOffset = 290; // Adjust this value as needed for the skills section
+            }
+
+            const elementPosition = targetElement.getBoundingClientRect().top;
+            const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+            window.scrollTo({
+              top: offsetPosition,
+              behavior: 'smooth'
+            });
+          }
+        });
+ });
+      
+  document.addEventListener('DOMContentLoaded', function () {
+    const modal = document.getElementById("myModal");
+    const modalImg = document.getElementById("img01");
+    const captionText = document.getElementById("caption");
+    const span = document.getElementsByClassName("close")[0];
+
+    document.querySelectorAll('.card-back a img').forEach(img => {
+      img.onclick = function (event) {
+        event.preventDefault();
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+      }
+    });
+
+    span.onclick = function () {
+      modal.style.display = "none";
+    }
+     modal.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  }
+  });
--- main.js
+++ main.js
@@ -5,4 +5,11 @@ var typed = new Typed(".text", {
     backDelay: 1000,
     loop: true
 })
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.querySelector('.hamburger');
+  const navbar = document.querySelector('.navbar');
 
+  hamburger.addEventListener('click', function () {
+    navbar.classList.toggle('active');
+  });
+});
